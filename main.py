bb79bee3667b9e9cdd37b4b2801cb2f19402d8d4
Nacho-punto <ignaciostamati5@gmail.com>
2025-09-10 13:52:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ user_admin="admin"
 password_admin="1234"
 
 
-
 claves = []
 letras_mayusculas = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','Á','É','Í','Ó','Ú','Ü','Ñ']
 letras_minusculas = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','á','é','í','ó','ú','ü','ñ']
